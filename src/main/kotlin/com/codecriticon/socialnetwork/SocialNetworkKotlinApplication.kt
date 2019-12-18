cfc8d4d10ab452881047e33aa30718fdc6d55575
Antonio Vidal <avidal@stratio.com>
2019-12-18 12:01:08 +0100

    Initial Commit

---
+++ src/main/kotlin/com/codecriticon/socialnetwork/SocialNetworkKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.codecriticon.socialnetwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialNetworkKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocialNetworkKotlinApplication>(*args)
+}
